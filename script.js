e45b11854df6a92acb2e9fc19ee89c00e7935288
hanayong <itkindyong@gmail.com>
2020-07-30 11:36:32 +0900

    test version

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("touchstart", function(){}, true);
+
+// Searching.Engine = function(){
+//     this.form.addEventListener('submit', e => {
+//         e.preventDefault();
+
+//         let engine = this.engine.vlaue
+//     });
+// }
+
+var num = 10;
+var str = 'Javascript';
+document.getElementById('text').innerHTML = (num == str);
